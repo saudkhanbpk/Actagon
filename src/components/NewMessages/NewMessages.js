ce8688656ed2ae623611206146fb53db8126a6f1
rehmanzeb <rehmanzeb1997@gmail.com>
2023-09-14 16:41:58 +0500

    chat screens ui

--- src/components/NewMessages/NewMessages.js
+++ src/components/NewMessages/NewMessages.js
@@ -1,33 +1,41 @@
-
 import React, { useState } from 'react';
-import {AiFillCheckCircle} from 'react-icons/ai'
+import { AiFillCheckCircle } from 'react-icons/ai';
+import { Link } from 'react-router-dom';
 
 function NewMessages() {
   // State to manage selected chats
   const [selectedChats, setSelectedChats] = useState([]);
+  // State to hold the search input value
+  const [searchInput, setSearchInput] = useState('');
 
   // Sample list of friends (replace with your data)
   const friends = [
     {
-      id: "1",
-      name: "Imran Khan",
-      image: "https://picsum.photos/200/300",
+      id: '1',
+      name: 'Imran Khan',
+      image: 'https://picsum.photos/200/300',
     },
     {
-      id: "2",
-      name: "Sameer Khan",
-      image: "https://picsum.photos/200/300",
+      id: '2',
+      name: 'Sameer Khan',
+      image: 'https://picsum.photos/200/300',
     },
     {
-      id: "3",
-      name: "Hamza Khan",
-      image: "https://picsum.photos/200/300",
+      id: '3',
+      name: 'Hamza Khan',
+      image: 'https://picsum.photos/200/300',
     },
     {
-      id: "4",
-      name: "Zeb Khan",
-      image: "https://picsum.photos/200/300",
+      id: '4',
+      name: 'Zaid Khan',
+      image: 'https://picsum.photos/200/300',
     },
+    {
+      id: '5',
+      name: 'Zaib Khan',
+      image: 'https://picsum.photos/200/300',
+    },
+    // Add more friends here...
   ];
 
   // Function to toggle chat selection on double click
@@ -39,6 +47,11 @@ function NewMessages() {
     }
   };
 
+  // Function to filter friends based on search input
+  const filteredFriends = friends.filter((friend) =>
+    friend.name.toLowerCase().includes(searchInput.toLowerCase())
+  );
+
   return (
     <div className="min-h-screen flex flex-col mt-2">
       {/* Header */}
@@ -46,50 +59,52 @@ function NewMessages() {
         <p className="text-blueButtonColor text-center text-lg font-semibold leading-5">
           Cancel
         </p>
-        <p className="text-textColorBlack text-center text-lg font-semibold leading-5">
+        <p className="text-textColorBlack text-center text-lg font-weight-[500] leading-5 font-sf">
           New Messages
         </p>
         <div>
-          <button className="text-blueButtonColor text-center text-lg font-semibold">
-            Create
-          </button>
+          <Link to="/group">
+            <button className="text-blueButtonColor text-center text-lg font-semibold">
+              Create
+            </button>
+          </Link>
         </div>
       </div>
 
       {/* Search Bar */}
-      
-            <div
-                className="flex justify-between items-center   mt-5  py-2 "
-                style={{ background: "rgba(118, 118, 128, 0.12)" }}
-            >
-                <div className="flex gap-1 items-center">
-                    <div>
-                        <input
-                            type="text"
-                            placeholder="Name:"
-                            className="flex p-3 w-full h-[30px] items-center bg-transparent "
-                            style={{ background: "transparent", outline: "none" }}
-                        />
-                    </div>
-                </div>
-
-            </div>
+      <div
+        className="flex justify-between items-center mt-4 py-2"
+        style={{ background: 'rgba(118, 118, 128, 0.12)' }}
+      >
+        <div className="flex gap-1 items-center">
+          <div>
+            <input
+              type="text"
+              placeholder="Name:"
+              className="flex p-3 w-full h-[30px] items-center bg-transparent"
+              style={{ background: 'transparent', outline: 'none' }}
+              value={searchInput}
+              onChange={(e) => setSearchInput(e.target.value)}
+            />
+          </div>
+        </div>
+      </div>
 
       {/* Selected Chats Count */}
-      <div className="flex self-start">
+      <div className="flex self-start mt-4">
         <h5 className="text-textColorBlack text-base font-semibold leading-5 ml-2 p-2">
           {selectedChats.length} Selected
         </h5>
       </div>
 
       {/* List of Friends */}
-      {friends.map((friend) => {
+      {filteredFriends.map((friend) => {
         const isSelected = selectedChats.includes(friend.id);
 
         return (
           <div
             className={`flex justify-between px-4 ${
-              isSelected ? "bg-gray-200" : ""
+              isSelected ? 'bg-[lightgray] border-md' : ''
             }`}
             key={friend.id}
             onDoubleClick={() => toggleChatSelection(friend.id)}
@@ -112,9 +127,11 @@ function NewMessages() {
               </div>
             </div>
             {isSelected && (
-              <div className='mt-6'>
+              <div className="mt-6">
                 {/* Replace this with your selected icon */}
-                <span ><AiFillCheckCircle className='text-blueButtonColor w-5 h-5'/></span>
+                <span>
+                  <AiFillCheckCircle className="text-blueButtonColor w-5 h-5" />
+                </span>
               </div>
             )}
           </div>
